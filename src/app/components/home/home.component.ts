5e6419f8c78d0c966e4fcf7da61032a39ba3e3d3
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-12-24 01:00:42 +0530

    updating UI of homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,6 +16,9 @@ export class HomeComponent {
   posts: string[] = [];
   comment: string = '';
 
+  selectedImage: File | null = null;
+  selectedVideo: File | null = null;
+
   constructor(private authService: AuthService) { }
 
   postSkill(): void {
@@ -34,5 +37,22 @@ export class HomeComponent {
     console.log(`Comment on ${post}: ${this.comment}`);
     this.comment = '';
   }
+
+
+
+
+  onFileSelected(event: Event, fileType: string): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      if (fileType === 'image') {
+        this.selectedImage = file;
+      } else if (fileType === 'video') {
+        this.selectedVideo = file;
+      }
+    }
+  }
+
+
 
 }
