63c5876cb66c89060e12eda0a63ab76510596408
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-12-27 11:14:14 +0530

    Remove vertical and horizantal scrollbar from homepage, Fixed the position of navbar and sidebar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,25 +1,69 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { LoadingComponent } from '../loading/loading.component';
+import { LoadingComponent } from "../loading/loading.component";
 import { CommonModule } from '@angular/common';
+import { TopNavbarComponent } from "../../shared/top-navbar/top-navbar.component";
+
+interface User {
+  name: string;
+  followers: number;
+  profilePic: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, TopNavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
+
+
 export class HomeComponent {
   newPost: string = '';
   posts: string[] = [];
   comment: string = '';
+  topUsers: User[] = [
+    {
+      name: 'Pradip Khandare',
+      followers: 120,
+      profilePic: 'assets/images/myPic.jpg'
+    },
+    {
+      name: 'Disha Gujrathi',
+      followers: 100,
+      profilePic: 'assets/images/myPic.jpg'
+    },
+    {
+      name: 'Shreyash Jadhav',
+      followers: 95,
+      profilePic: 'assets/images/myPic.jpg'
+    },
+    {
+      name: 'Sulakshana Pawar',
+      followers: 97,
+      profilePic: 'assets/images/myPic.jpg'
+    },
+    {
+      name: 'Rutuja Tathe',
+      followers: 89,
+      profilePic: 'assets/images/myPic.jpg'
+    }
+
+  ];
+
+
 
   selectedImage: File | null = null;
   selectedVideo: File | null = null;
 
   constructor(private authService: AuthService) {}
+
+
+  ngOnInit() {
+
+  }
 
   postSkill(): void {
     if (this.newPost.trim()) {
