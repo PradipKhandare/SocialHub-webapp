8f6ecbe12f519e87aef32cdf025af1800fd2fa15
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-12-29 00:15:27 +0530

    fixed videos UI

--- src/app/components/community-join-screen/community-join-screen.component.ts
+++ src/app/components/community-join-screen/community-join-screen.component.ts
@@ -3,26 +3,29 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatCommonModule } from '@angular/material/core';
 import { MatDialogContent, MatDialogModule } from '@angular/material/dialog';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-community-join-screen',
   standalone: true,
-  imports: [MatDialogModule, MatDialogContent, CommonModule,
+  imports: [MatDialogModule, CommonModule,
     RouterModule,
-    MatFormFieldModule,
-  ],
+    MatFormFieldModule],
   templateUrl: './community-join-screen.component.html',
   styleUrl: './community-join-screen.component.scss'
 })
 export class CommunityJoinScreenComponent {
-onClickOfCancelButton() {
-throw new Error('Method not implemented.');
-}
-onClickOfJoinButton() {
-throw new Error('Method not implemented.');
-}
+
+  constructor(private router: Router) { }
+
+  onClickOfCancelButton() {
+    this.router.navigate(['/neutrance']);
+  }
+
+  onClickOfJoinButton() {
+    throw new Error('Method not implemented.');
+  }
 
 
 
